2996c07a4f2522d63789600c95418916a044b653
deniselarsson <denisee.larsson@gmail.com>
2021-10-26 21:46:21 +0200

    first react stop watch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import react from "react";
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  //2 variabel function
+  //time track second of time 
+  //setTime to set the time
+  const [time, setTime] = React.useState(0) //0 default value
+  const [timerOn, setTimeOn] = React.useState(false)
+
+  //useEffect takes an arrayfunction 
+  //it will run as soon as the component is rendered
+  react.useEffect(() => {
+    //the logig everytime the timer is on and when it is off
+    let interval = null;
+
+    //if we turn it on 
+    if (timerOn){
+      interval = setInterval(()=>{
+        setTime(prevTime => prevTime + 10) 
+      },10)
+    //else when we turn it off
+    }else{
+      clearInterval(interval)
+    }
+    return ()  => clearInterval(interval)
+  }, [timerOn]) // it will run every time the time changes
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+      <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+      <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+    </div>
+    {!timerOn && time == 0 && (
+      <button onClick={()=> setTimeOn(true)}>Start</button>
+    )}
+    {timerOn &&(
+       <button onClick={()=> setTimeOn(false)}>Stop</button>
+    )}
+    {!timerOn && time !== 0 &&(
+      <button onClick={()=> setTimeOn(true)}>Resume</button>
+    )}
+    {!timerOn && time > 0 &&(
+      <button onClick={()=> setTime(0)}>Reset</button>
+    )}
     </div>
   );
 }
