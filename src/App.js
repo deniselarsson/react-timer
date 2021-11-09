4989170f2bf96a28b913958ffe553f2a606ee070
Denisé Larsson <denisee.larsson@gmail.com>
2021-11-09 20:57:31 +0100

    added some comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import './App.css';
 
 function App() {
 
+  //variabel time som vi kan tracka tiden
   const [time, setTime] = React.useState(0) //0 default value
   const [timerOn, setTimeOn] = React.useState(false)
 
@@ -15,34 +17,32 @@ function App() {
     if (timerOn){
       interval = setInterval(()=>{
         setTime(prevTime => prevTime + 10) 
-      },10)
+      },10) //1000 är 1 sek, 100 är 0.1sek, 10 är 0.01 sek
     //else when we turn it off
     }else{
       clearInterval(interval)
     }
     return ()  => clearInterval(interval)
-  }, [timerOn]) // it will run every time the time changes
+  }, [timerOn]) // it will run every time the time changes in an array
+
+  //slice -2 gör att vi bara har 2 siffror 
+  //rad 36, 1000millisekund är 1 sekund, 60 sek i 1min
+  //rad 35, 60000millisekund är 1 min, 60 sek i 1min
+  //math.floor avrunda till heltal
 
   
   return (
     <div className="App">
+      <h1>Stop Watch</h1>
     <div>
-      <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+      <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span> 
       <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
       <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
     </div>
-    {!timerOn && time === 0 && (
       <button onClick={()=> setTimeOn(true)}>Start</button>
-    )}
-    {timerOn &&(
-       <button onClick={()=> setTimeOn(false)}>Stop</button>
-    )}
-    {!timerOn && time !== 0 &&(
+      <button onClick={()=> setTimeOn(false)}>Stop</button>
       <button onClick={()=> setTimeOn(true)}>Resume</button>
-    )}
-    {!timerOn && time > 0 &&(
       <button onClick={()=> setTime(0)}>Reset</button>
-    )}
     </div>
   );
 }
