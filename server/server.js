9d5b7e12a55ea2ae458902f9ffac9fa29d3f3871
Denisé Larsson <denisee.larsson@gmail.com>
2021-11-15 22:00:20 +0100

    deleted comments

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-const express = require('express') //import express
+const express = require('express')
 const app = express()
 
-//set up a rout for the api 
 app.get("/api", (req,res) => {
-    res.json({"users":["userOne", "userTwo", "userThree"]}) //this is our backend api, gonna fetch users
+    res.json({"users":["userOne", "userTwo", "userThree"]}) 
 })
-//in order to start up our backend 
 app.listen(5000,  ()=>{
     console.log("Server is up and running on 5000")
 })
